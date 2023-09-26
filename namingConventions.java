b74b49b7d4b4c3a37488c82165a0063ddf6012cd
Om Srivastava <79087911+omsrivastava512@users.noreply.github.com>
2023-09-26 17:27:32 +0530

    Create 1. namingConventions.java

---
+++ namingConventions.java
@@ -0,0 +1,33 @@
+/* Introduction to cases and naming conventions */
+
+//camelCase:  The first letter of each word is capitalized except the first word
+/*Used for variable, objects and methods*/
+int newVariable1;
+void newMethod(){}
+ClassName objectName = new ClassName();
+
+
+//PascalCase:  The first letter of each word is capitalized
+/*Used for class and interface*/
+class ThisIsAClass{}
+interface NewInterface{}
+
+//UPPER_CASE_SNAKE_CASE: Used for constants
+final int PI_VALUE = 22/7;
+final int MIN_AGE=18;
+
+/*--Avoid These--*/
+//snake_case: all lowercase letters with words separated by underscore
+int this_one_is_annoying;
+
+//kebab-case: words separated by hyphen
+int do-not-use-these;
+
+/*----------------------------------------------------------------------------*/
+//Exercise: Define these sentences
+  //1.
+  String myString = new String("Hello, World!");
+
+
+
+
