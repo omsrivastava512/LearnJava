13e9d3b24baa1bd113b3e9429047745d5ee3fa39
Om Srivastava <79087911+omsrivastava512@users.noreply.github.com>
2023-10-05 21:08:58 +0530

    Update parseDouble.java

--- Programs/parseDouble.java
+++ Programs/parseDouble.java
@@ -1,82 +1,82 @@
-// Function to convert string into double
+/******************************************************************************
+Program to Convert Numeric values from String to Double
 
-class 
+*******************************************************************************/
+import java.io.Console;
+public class Main
+{   static String c = new String();
 
-/* Method to check if the given string contains valid numerical characters */
-bool check(const char *c){
+    static boolean check(final String c){
     int i=0; 				//using i as an index variable to access array characters
-    if(c[i]=='-'||c[i]=='+')
+    while (c.charAt(i) == ' ') {
+            i++;
+        }
+    if(c.charAt(i)=='-'||c.charAt(i)=='+')
         i++;				//increment i value if the user has input a signed number
     
   // Iterate through the string to check for valid numerical characters
-// Using do-while instead of while so in case of null input, the method does not return true by default
-    do{            
-      if(c[i]<'.' || c[i]>'9')		//note: '\0' < '0' which is 0 < 48
-      	return false;
-      	if(c[i]=='/') 			//since '/' lies b/w '.' and '9' in the character map
+    while(i<c.length())			
+    {            
+      if(c.charAt(i)>='.' || c.charAt(i)<='9')		
+      	return true;
+      	if(c.charAt(i)=='/') 			//since '/' lies b/w '.' and '9' in the character map
       	return false;
       	i++;
     }
-    while(c[i]!='\0');			//  since a string is a null-terminated array
-	return true;		
+    //  
+	 return false;		
 }
-
 /* Convert the given string to a double */
-double stringToDouble(const char* c) {
+static double stringToDouble(final String c) {
     double result = 0;
     double decimal=0;  		 
     int sign = 1; 		// Initialize sign as positive
-	
+	int i=0;
         // Skip white spaces
-        while (*c == ' ') {
-            c++;
-        }
-        if (*c == '-' || *c == '+') {
-            sign = (*c=='-')? -1:1;
-            c++;
+        while (c.charAt(i) == ' ') {
+            i++;
         }
         // Handle signs
-        
-if(!check(c)) return 0;
-        
-        // Convert characters to double
-        while (*c!='.') {          // Eliminates when receives the first decimal '.'         
-           if(*c=='\0')		// If no decimal present
-           goto a;
-            result = result * 10 + (*c - '0');
-            c++;
+        if (c.charAt(i) == '-' || c.charAt(i) == '+') {
+            sign = (c.charAt(i)=='-')? -1:1;
+            i++;
         }
         
-        const char* ptr =++c; 		// Stores the address of where the decimal numbers start
+      
 
-	// Storing decimal numbers 
-        while(*c!='\0'){		
-            if(*c=='.')			// If receives another decimal '.'
-            break;
-            decimal = decimal*10 + (*c - '0');
-            c++;
+if(!check(c)) return 0;
+        int j=c.length();
+        // Convert characters to double
+        while ((i)<c.length()) {            
+        
+           if(c.charAt(i)=='.')	{       	// Decimal found
+               j=i;
+           result+=.2;      // Exception
+           }
+            result = result * 10 + ((int)c.charAt(i) - '0');
+            i++;
         }
-        while(*ptr!='\0'){		
-            if(*ptr=='.')		// Same reason as above
+        
+    j++;
+	// Storing decimal numbers 
+   
+        while(j<c.length()){		
+            if(c.charAt(i)=='.')		// Found another decimal
             break;
             decimal=decimal/10;		// Placing decimal before the numbers
-            ptr++;
+            j++;
         }
         
-        a:
+        
         // Apply sign
         return sign * (result+decimal) ;
     
 }
 
-/* Use your code here */
-int main(){
-  char a[20];
-  printf("/* This program converts numbers from a String into a floating point number */\n"); 
-  printf("/* Enter a non numerical value or 0 to exit the loop */\n"); 
-  do{
-  scanf("%s",a);
-  printf(":%f\n",stringToDouble(a));
-  } while(stringToDouble(a));
-  return 0;
+	public static void main(String[] args) {
+	    
+	    c= System.console().readLine(":");
+	    
+		System.out.println(stringToDouble(c));
+	}
 }
